53afb5902917331240f46e6d8a1ead01e6bff09f
Murilo Silva <muriloedupenha@gmail.com>
2026-03-20 16:34:37 -0300

    Ajuste para receber array de errados

--- main.py
+++ main.py
@@ -1,33 +1,73 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Body
 import pandas as pd
 import uuid
 from io import BytesIO
 from datetime import datetime
+import json
+
 from bot import rodar_bot, criar_pasta_downloads
 
 app = FastAPI()
 
 
-@app.post("/upload-xlsx/")
-async def upload_xlsx(file: UploadFile = File(...)):
+@app.post("/processar/")
+async def processar(
+    file: UploadFile = File(None),
+    chaves: str = Body(default=None)
+):
     file_id = str(uuid.uuid4())
 
     try:
-        contents = await file.read()
-        df = pd.read_excel(BytesIO(contents), header=None)
+        lista_chaves = []
 
-        df = df.dropna()
-        chaves = df.iloc[:, 0].astype(str).tolist()
+        if file:
+            contents = await file.read()
+            df = pd.read_excel(BytesIO(contents), header=None)
 
-        if not chaves:
-            raise HTTPException(status_code=400, detail="Arquivo vazio")
+            df = df.dropna()
+            lista_chaves = df.iloc[:, 0].astype(str).tolist()
 
-        print(f"[{file_id}] Total: {len(chaves)}")
+        elif chaves:
+            chaves = chaves.strip()
+
+            if chaves.startswith("["):
+                try:
+                    lista_chaves = json.loads(chaves)
+
+                    if not isinstance(lista_chaves, list):
+                        raise ValueError()
+
+                except:
+                    raise HTTPException(
+                        status_code=400,
+                        detail="JSON inválido"
+                    )
+
+            else:
+                lista_chaves = [
+                    c.strip() for c in chaves.split(",") if c.strip()
+                ]
+
+        else:
+            raise HTTPException(
+                status_code=400,
+                detail="Envie um arquivo ou uma lista de chaves"
+            )
+
+        lista_chaves = [str(c) for c in lista_chaves]
+
+        if not lista_chaves:
+            raise HTTPException(
+                status_code=400,
+                detail="Nenhuma chave válida"
+            )
+
+        print(f"[{file_id}] Total: {len(lista_chaves)}")
 
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         pasta_execucao = criar_pasta_downloads(f"xmls_{timestamp}")
 
-        resultado = rodar_bot(chaves, pasta_execucao)
+        resultado = rodar_bot(lista_chaves, pasta_execucao)
 
         return resultado
 
